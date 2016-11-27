9a2ec3b5086af2e2dcc66dce5f39c2afc42d2a5e
lijing <njlijing1024@163.com>
2016-11-27 17:03:52 +0800

    init

---
+++ src/main/java/ocr/common/App.java
@@ -0,0 +1,17 @@
+package ocr.common;
+
+
+/**
+ * TODO: ocr公共库
+ * @date 2016年11月26日
+ * @author lijing@yonyou.com
+ */
+public class App
+{
+	
+    public static void main( String[] args )
+    {
+    	
+    }   
+
+}
