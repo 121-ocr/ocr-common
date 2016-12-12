c79f8365de6262c8c98ca9266969a2dce7d821c7
window <window@10.16.122.22>
2016-12-12 14:05:37 +0800

    业务组件对外公布常量

---
+++ src/main/java/orc/common/busi/open/inventorycenter/InvBusiOpenContant.java
@@ -0,0 +1,13 @@
+package orc.common.busi.open.inventorycenter;
+
+/**
+ * 业务组，公开对外常量类
+ * 
+ * @author LCL
+ *
+ */
+public class InvBusiOpenContant {
+	// invfacility-mgr 对外提供查询货位和商品关系地址
+ 	public static final String LOCATIONSADDRESS = "queryLocatonsGoodsRelation";
+
+}
