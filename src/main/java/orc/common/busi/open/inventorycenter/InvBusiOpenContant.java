716e5f7d294316f4c567cf9533f0e315693fdadb
window <window@10.16.122.12>
2016-12-17 10:08:44 +0800

    提交

--- src/main/java/orc/common/busi/open/inventorycenter/InvBusiOpenContant.java
+++ src/main/java/orc/common/busi/open/inventorycenter/InvBusiOpenContant.java
@@ -7,18 +7,19 @@ package orc.common.busi.open.inventorycenter;
  *
  */
 public class InvBusiOpenContant {
-	// invfacility-mgr 对外提供查询货位和商品关系地址
-	
-	public static final String FACILITYCOMPONTENNAME = "invfacility-mgr";
-	
- 	public static final String LOCATIONSADDRESS = "queryLocatonsGoodsRelation";
+	// invfacility-mgr 对外提供查询货架
 
- 	
- 	
- 	//--预留组件
-	
+	public static final String FACILITYCOMPONTENNAME = "invfacility-mgr";
+
+	// locationrelation 对外提供查询货位和商品关系地址
+	public static final String LOCATIONRELATIONCOMPONTENNAME = "locationrelation-mgr";
+
+	public static final String LOCATIONSADDRESS = "queryLocatonsGoodsRelation";
+
+	// --预留组件
+
 	public static final String RESERVEDCOMPONTENNAME = "stockreserved";
-	
- 	public static final String QUERYRESERVEDSADDRESS = "getFirstBatch";
- 	
+
+	public static final String QUERYRESERVEDSADDRESS = "getFirstBatch";
+
 }
