4f15787cdbdd89ef2f1ccfbd12c8ead59651d177
lijing <njlijing1024@163.com>
2016-12-19 19:32:55 +0800

    修改
    Signed-off-by: lijing <njlijing1024@163.com>

--- src/main/java/ocr/common/handler/SampleBillBaseHandler.java
+++ src/main/java/ocr/common/handler/SampleBillBaseHandler.java
@@ -49,14 +49,13 @@ public class SampleBillBaseHandler extends ActionHandlerImpl<JsonObject> {
 		
 		String boId = bo.getString("bo_id");
 		//TODO 如果没有boid，则调用单据号生成规则生成一个单据号		
-		//交易单据一般要记录协作方
-    	String partnerAcct = getPartnerAcct(bo);
+
 		// 当前操作人信息
 		JsonObject actor = ActionContextTransfomer.fromMessageHeaderToActor(headerMap);
 
 		// 记录事实对象（业务数据），会根据ActionDescriptor定义的状态机自动进行状态变化，并发出状态变化业务事件
 		// 自动查找数据源，自动进行分表处理
-		this.recordFactData(appActivity.getBizObjectType(), bo, boId, actor, partnerAcct, null, result -> {
+		this.recordFactData(appActivity.getBizObjectType(), bo, boId, actor, null, result -> {
 			if (result.succeeded()) {				
 				//后续处理
 				afterProcess(bo, ret -> {
@@ -119,9 +118,6 @@ public class SampleBillBaseHandler extends ActionHandlerImpl<JsonObject> {
 		future.complete(msg.body());		
 	}
 
-	public String getPartnerAcct(JsonObject bo) {
-		return null;
-	}	
 
 	@Override
 	public String getEventAddress() {
